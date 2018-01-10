847095f91dcdc06413f466fbdbc405a231003b4a
tatsuro <tatsuro.saito@s-cubism.jp>
2018-01-10 15:15:24 +0900

    added logout function.

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   patch 'delete' => 'interview#delete'
 
   devise_for :users
+  devise_scope :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

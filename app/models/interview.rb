82580fc459e6d6ea49e5bdaeffa3d5f86c5b2e32
tatsuro <tatsuro.saito@s-cubism.jp>
2018-01-10 10:54:23 +0900

    fixed miss

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,5 @@
 class Interview < ApplicationRecord
   enum status: { pending: 0, approved: 1, rejected: 2 , deleted: 3}
   belongs_to :user, class_name: 'User', :foreign_key => 'users_id'
+  belongs_to :recipient, class_name: 'User', :foreign_key => 'recipient_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum sex: { other: 0, male: 1, famale: 2 }
-  has_many :interview   
+  has_many :user_interview, class_name: 'Interview', :foreign_key => 'users_id'
+  has_many :recipient_interview, class_name: 'Interview', :foreign_key => 'recipient_id'      
 end

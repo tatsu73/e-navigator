e84c283db7fe3e083be0fe2e005f2a88a851f0b1
tatsuro <tatsuro.saito@s-cubism.jp>
2018-01-10 14:34:34 +0900

    fixed according to review.

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem "jquery-rails"
 gem 'bootstrap-sass'
+gem 'dotenv-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 	   address:               'smtp.gmail.com',
 	   port:      	           587,
 	   domain:                'smtp.gmail.com',
-	   user_name:             'testtatsu52@gmail.com',
-	   password:              'testtatsu',
+	   user_name:             ENV['MAIL_ID'],
+	   password:              ENV['MAIL_PASSWORD'],
 	   authentication:        'plain',
 	   enable_starttls_auto:  true
   }
